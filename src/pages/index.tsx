4f1cb0f3e628763fcc817807d1f5a114eb359742
Dhiego Gonçalves da Silva <dhiego.silva@b2wdigital.com>
2022-01-29 17:47:59 -0400

    Adicionando fonts e implementando o style global.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
-import styles from '../styles/home.module.scss';
+import Head from 'next/head';
 
 export default function Home() {
-  return <h1 className={styles.title}>IGNEWS</h1>;
+  return (
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <h1>IGNEWS</h1>
+    </>
+  );
 }
